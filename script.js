477568445aefefa1310af5050bd6029b943a1547
krbrennan <krb.brennan@gmail.com>
2018-08-07 14:20:16 -0400

    about to write parser for handling form submission data

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
+const searchField = document.getElementById('searchInput')
+
+const temp = document.getElementById('temp')
+const main = document.getElementById('main')
+const forecast = document.getElementById('forecast')
 // api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}
 
 // const url = 'api.openweathermap.org/data/2.5/weather?zip=18020,us'
 
-async function getWeather(){
-  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=18020&APPID=ac628c4c599f5392d529f22369c7f85f')
-  const data = await response.json()
+searchField.addEventListener('submit', function(e){
+  e.preventDefault();
+  e.persist;
+  handleForm(e.target[0].value);
+})
+
+function handleForm(e) {
+  console.log(e)
 }
+
+async function getWeather(){
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=18020&APPID=ac628c4c599f5392d529f22369c7f85f&units=imperial')
+  return results(await response.json())
+}
+
+function capitalize(data){
+
+  let info = data.split(" ").map(function(ele){
+    return ele[0].toUpperCase() + ele.substr(1)
+    // console.log(ele)
+  });
+  return info.join(" ")
+}
+
+function results(data){
+  // console.log(data)
+  temp.innerHTML = Math.floor(data.main.temp)
+  main.innerHTML = data.weather[0].main
+  forecast.innerHTML = capitalize(data.weather[0].description)
+
+
+}
+
+// window.onload = getWeather();
