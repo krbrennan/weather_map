04762de00aa0eadf3a1b48472663ca07de49bb25
krbrennan <krb.brennan@gmail.com>
2018-08-08 13:51:04 -0400

    adding forecast functionality

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 const searchField = document.getElementById('searchInput');
 
+
+// current weather
 const temp = document.getElementById('temp');
 const tempIcon = document.getElementById('temp-icon');
 const high = document.getElementById('high');
 const low = document.getElementById('low');
 const humidity = document.getElementById('humidity');
-
+//
 const main = document.getElementById('main');
 const forecast = document.getElementById('forecast');
 const cityName = document.getElementById('city-name');
 const getGeo = document.getElementById('geo-locate-btn');
-// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}
 
-// const url = 'api.openweathermap.org/data/2.5/weather?zip=18020,us'
+// forecast
+const todayTemp = document.getElementById('today-temp')
+const todayTempIcon = document.getElementById('today-temp-icon')
+const todayHighLow = document.getElementById('today-high-low')
+const todayHigh = document.getElementById('today-high')
+const todayLow = document.getElementById('today-low')
+const todayHumidityDiv = document.querySelector('today-humidity')
+const todayHumidity = document.getElementById('today-humidity')
+
+const tonightTemp = document.getElementById('tonight-temp')
+const tonightTempIcon = document.getElementById('tonight-temp-icon')
+const tonightHighLow = document.getElementById('tonight-high-low')
+const tonightHigh = document.getElementById('tonight-high')
+const tonightLow = document.getElementById('tonight-low')
+
+const tomorrowTemp = document.getElementById('tomorrow-temp')
+const tomorrowTempIcon = document.getElementById('tomorrow-temp-icon')
+const tomorrowHighLow = document.getElementById('tomorrow-high-low')
+const tomorrowHigh = document.getElementById('tomorrow-high')
+const tomorrowLow = document.getElementById('tomorrow-low')
+
+
+
+
 
 searchField.addEventListener('submit', function(e){
   e.preventDefault();
@@ -27,6 +51,7 @@ getGeo.addEventListener('click', function(e){
 
 function handleForm(e) {
   getWeather(e)
+  getForecast(e)
 }
 
 // async function getWeatherZip(e){
@@ -45,6 +70,17 @@ async function getWeatherCoords(lat,long){
   return results(await response.json())
 }
 
+async function getForecast(e){
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${e},us&APPID=ac628c4c599f5392d529f22369c7f85f&units=imperial`)
+  return forecastResults(await response.json())
+}
+
+async function getForecastCoords(lat, long){
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&APPID=ac628c4c599f5392d529f22369c7f85f&units=imperial`)
+  return forecastResults(await response.json())
+}
+
+
 function capitalize(data){
   // capitalizes the forecast. default is all lower-case
   let info = data.split(" ").map(function(ele){
@@ -53,8 +89,37 @@ function capitalize(data){
   return info.join(" ")
 }
 
-function results(data){
+function forecastResults(data){
+  // console.log(data.list[0].weather[0].icon)
   console.log(data)
+// Today
+  let temperatureIcon = data.list[0].weather[0].icon
+  todayTempIcon.src = `http://openweathermap.org/img/w/${temperatureIcon}.png`
+  todayTempIcon.style.visibility = 'visible'
+
+  todayHigh.innerHTML = Math.floor(data.list[0].main.temp_max) + "˚"
+  todayHigh.style = 'border-right: 2px solid black;'
+  todayLow.innerHTML = Math.floor(data.list[0].main.temp_min)+ "˚"
+
+// Tonight
+  let tonightTemperatureIcon = data.list[2].weather[0].icon
+  tonightTempIcon.src = `http://openweathermap.org/img/w/${tonightTemperatureIcon}.png`
+  tonightTempIcon.style.visibility = 'visible'
+
+  tonightLow.innerHTML = Math.floor(data.list[2].main.temp_min)+ "˚"
+
+// Tomorrow
+  let temperatureIcon = data.list[0].weather[0].icon
+  todayTempIcon.src = `http://openweathermap.org/img/w/${temperatureIcon}.png`
+  todayTempIcon.style.visibility = 'visible'
+
+  todayHigh.innerHTML = Math.floor(data.list[0].main.temp_max) + "˚"
+  todayHigh.style = 'border-right: 2px solid black;'
+  todayLow.innerHTML = Math.floor(data.list[0].main.temp_min)+ "˚"
+}
+
+function results(data){
+  // console.log(data)
   cityName.innerHTML = data.name
   cityName.style.visibility = 'visible'
 
@@ -62,11 +127,11 @@ function results(data){
   tempIcon.src = `http://openweathermap.org/img/w/${temperatureIcon}.png`
   tempIcon.style.visibility = 'visible'
 
-  high.innerHTML = Math.floor(data.main.temp_max) + "˚"
-  high.style = 'border-right: 2px solid black;'
-  low.innerHTML = Math.floor(data.main.temp_min)+ "˚"
+  // high.innerHTML = Math.floor(data.main.temp_max) + "˚"
+  // high.style = 'border-right: 2px solid black;'
+  // low.innerHTML = Math.floor(data.main.temp_min)+ "˚"
 
-  humidity.innerHTML = data.main.humidity + "%"
+  humidity.innerHTML = data.main.humidity + "%" + "Humidity"
 
   temp.innerHTML = Math.floor(data.main.temp)
   main.innerHTML = data.weather[0].main
@@ -84,5 +149,6 @@ function getCoords(){
     let lat = position.coords.latitude;
     let long = position.coords.longitude;
     getWeatherCoords(lat,long);
+    getForecastCoords(lat,long);
   });
 }
