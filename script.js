f46e80c87513985bb290e62feac94d6627882297
krbrennan <krb.brennan@gmail.com>
2018-08-08 12:06:53 -0400

    geolocator and zip and city lookup work. Now to work on styling

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const searchField = document.getElementById('searchInput');
 
 const temp = document.getElementById('temp');
+const tempIcon = document.getElementById('temp-icon');
+const high = document.getElementById('high');
+const low = document.getElementById('low');
+const humidity = document.getElementById('humidity');
+
 const main = document.getElementById('main');
 const forecast = document.getElementById('forecast');
 const cityName = document.getElementById('city-name');
@@ -21,22 +26,17 @@ getGeo.addEventListener('click', function(e){
 });
 
 function handleForm(e) {
-  // console.log(e)
-  // if Number(e) evaluates to typeof number, getWeatherZip
-  // if Number(e) evaluates to NaN, getWeather
-  if(typeof e === "number"){
-    getWeatherZip(e)
-  } else {getWeatherCity(e)}
+  getWeather(e)
 }
 
-async function getWeatherZip(e){
-  const zip = Number(e)
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${zip},US&type=accurate&APPID=ac628c4c599f5392d529f22369c7f85f&units=imperial`)
-  return results(await response.json())
-}
+// async function getWeatherZip(e){
+//   const zip = Number(e)
+//   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=ac628c4c599f5392d529f22369c7f85f&units=imperial`)
+//   return results(await response.json())
+// }
 
-async function getWeatherCity(e){
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e}&APPID=ac628c4c599f5392d529f22369c7f85f&units=imperial`)
+async function getWeather(e){
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e},us&APPID=ac628c4c599f5392d529f22369c7f85f&units=imperial`)
   return results(await response.json())
 }
 
@@ -46,25 +46,32 @@ async function getWeatherCoords(lat,long){
 }
 
 function capitalize(data){
-
+  // capitalizes the forecast. default is all lower-case
   let info = data.split(" ").map(function(ele){
     return ele[0].toUpperCase() + ele.substr(1)
-    // console.log(ele)
   });
   return info.join(" ")
 }
 
 function results(data){
   console.log(data)
+  cityName.innerHTML = data.name
+  cityName.style.visibility = 'visible'
+
+  let temperatureIcon = data.weather[0].icon
+  tempIcon.src = `http://openweathermap.org/img/w/${temperatureIcon}.png`
+  tempIcon.style.visibility = 'visible'
+
+  high.innerHTML = Math.floor(data.main.temp_max) + "˚"
+  high.style = 'border-right: 2px solid black;'
+  low.innerHTML = Math.floor(data.main.temp_min)+ "˚"
+
+  humidity.innerHTML = data.main.humidity + "%"
+
   temp.innerHTML = Math.floor(data.main.temp)
   main.innerHTML = data.weather[0].main
   forecast.innerHTML = capitalize(data.weather[0].description)
-  cityName.innerHTML = data.name
 }
-
-// window.onload = getWeather();
-
-
 
 //
 //
